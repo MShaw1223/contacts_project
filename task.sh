47193867a84a74b6909db558fd36ab862a7161b7
Miller <millershaw61@gmail.com>
2025-01-21 15:46:32 +0000

    Feat: Send and read messages
    Send messages fully functional, read messages has 2/3 functions. View latest message is a WIP.

--- task.sh
+++ task.sh
@@ -1,14 +1,120 @@
+#! /bin/bash
 cf="contacts.txt"
-if ! -f $cf; then
+mf="messages"
+if [ ! -f $cf ]; then
 	touch "$cf"
 fi
+if [ ! -d $mf ]; then
+	mkdir "$mf"
+fi
+
+addContact() {
+        read -p "Enter the name = " name
+        if grep -q "^$name:" "$cf" ;then
+                echo "id = $name alredy exists"
+        fi
+
+        read -p "Enter the number = " number
+
+        if grep -q ",$number," "$cf" ;then
+                echo "$number alredy exists"
+        fi
+
+        echo "$name:$number" >> "$cf"
+	return 0
+}
+
+sendMessage(){
+	read -p "Enter your phone number: " sender
+	read -p "Enter recipient phone number: " recipient
+	read -p "Enter the message: " message_body
+	send_time=$(date)
+
+	if [ ! -d $recipient ]; then
+		mkdir "$mf/$recipient"
+	fi
+
+	base="$mf/$recipient"
+	if [ ! -f $sender ]; then
+		touch "$base/$sender"
+	fi
+
+	echo "$send_time:$message_body" >> "$base/$sender"
+
+	return 0
+}
+viewMessage(){
+	read -p "Enter your phone number: " user
+	echo -e "1. View Sent Messages.\n2. View Recieved Messages.\n3. View latest message."
+	read -p "> " ch
+
+	recipients=$(ls $mf)
+	recip_arr=($chats)
+
+	recieval_base="$mf/$user"
+
+	case $ch in
+		1)
+			# sent chats
+			for recip in ${recip_arr[@]};do
+				curr_chat="$mf/$recip/$user"
+				if [ -f "$curr_chat" ]; then
+					echo "-- Sent to: $recip --"
+					cat "$curr_chat"
+					echo "-- -- -- -- -- -- --"
+				fi
+			done;;
+		2)
+			user_chats=$(ls "$recieval_base")
+
+			uc_array=($user_chats)
+
+			for chat in ${user_chats[@]}; do 
+				echo "-- Recieved from: $chat --"
+				cat "$recieval_base/$chat"
+				echo "-- -- -- -- -- -- -- -- --"
+			done;;
+		3)
+			# tail -n1 for last message
+			user_chats=$(ls "$recieval_base")
+
+			uc_array=($user_chats)
+
+			latest_each=()
+			for chat in ${user_chats[@]}; do 
+				iter_latest=$(tail -n 1 "$recieval_base/$chat")
+				latest_each[${#latest_each[@]}]="$iter_latest"
+				# latest_each+=("$iter_latest")
+			done
+
+			# change back to colon
+			latest=$(echo ${latest_each[0]} | awk -F '|' '{print $1}' | date -d +%s)
+			echo $latest_date
+			for l in "${latest_each[@]}"; do
+				echo "lat= $latest l=$l"
+				current=$(echo $l | awk -F '|' '{print $1}' | date -d +%s)
+				if [ $latest -lt $current ];then
+					latest=$current
+				fi
+			done
+			echo $latest;;
+		*)
+			echo "Incorrect input."
+			return 1;;
+	esac
+}
+
 
 while true; do
-	echo -e 1. Add Contactn2. Display Contactsn3. Delete Contactn4. Update Contactn5. Search Contactn6. Send messagen7. View Messagesn8. Quit
+	echo -e "1. Add Contact\n2. Display Contacts\n3. Delete Contact\n4. Update Contact\n5. Search Contact\n6. Send message\n7. View Messages\n8. Quit"
 	read -p  "> " ch
 	 case $ch in
 		 1)
-			 addContact;;
+			 if ! addContact; then
+				 echo "Unable to add Contact"
+			 else
+				 echo "Contact added Successfully!"
+			 fi;;
 		 2)
 			 displayContact;;
 		 3)
