550c5370275b8f462da3abab86e6921acfd4c089
Miller Shaw <134694154+MShaw1223@users.noreply.github.com>
2025-01-21 16:50:27 +0000

    Update contacts.sh: include new Message Functions

--- contacts.sh
+++ contacts.sh
@@ -85,3 +85,85 @@ searchContact() {
                 esac
 
 }
+sendMessage(){
+	read -p "Enter your phone number: " sender
+	read -p "Enter recipient phone number: " recipient
+	read -p "Enter the message: " message_body
+	send_time=$(date)
+
+	if [ ! -d "$mf/$recipient" ]; then
+		mkdir "$mf/$recipient"
+	fi
+
+	base="$mf/$recipient"
+	if [ ! -f "$base/$sender" ]; then
+		touch "$base/$sender"
+	fi
+
+	echo "$send_time:$message_body" >> "$base/$sender"
+
+	return 0
+}
+viewMessage(){
+	read -p "Enter your phone number: " user
+	echo -e "1. View Sent Messages.\n2. View Recieved Messages.\n3. View latest message."
+	read -p "> " ch
+
+	recipients=$(ls $mf)
+	recip_arr=($chats)
+
+	recieval_base="$mf/$user"
+
+	case $ch in
+		1)
+			# sent chats
+			for recip in ${recip_arr[@]};do
+				curr_chat="$mf/$recip/$user"
+				if [ -f "$curr_chat" ]; then
+					echo "-- Sent to: $recip --"
+					cat "$curr_chat"
+					echo "-- -- -- -- -- -- --"
+				fi
+			done;;
+		2)
+			user_chats=$(ls "$recieval_base")
+
+			uc_array=($user_chats)
+
+			for chat in ${user_chats[@]}; do 
+				echo "-- Recieved from: $chat --"
+				cat "$recieval_base/$chat"
+				echo "-- -- -- -- -- -- -- -- --"
+			done;;
+		3)
+			# tail -n1 for last message
+			user_chats=$(ls "$recieval_base")
+
+			uc_array=($user_chats)
+
+			latest_each=()
+			for chat in ${user_chats[@]}; do 
+				iter_latest=$(tail -n 1 "$recieval_base/$chat")
+				latest_each[${#latest_each[@]}]="$iter_latest"
+				# latest_each+=("$iter_latest")
+			done
+
+			latest_unformatted=$(echo ${latest_each[0]} | awk -F : '{print $1}')
+			latest=$(date -d "$latest_unformatted" +%s)
+			latestDates=(["$latest"]=$latest_unformatted)
+			for l in "${latest_each[@]}"; do
+				current_unf=$(echo $l | awk -F : '{print $1}')
+				current=$(date -d "$current_unf" +%s)
+
+				latestDates["$current"]=$l
+
+				if [ $latest -lt $current ];then
+					latest=$current
+				fi
+			done
+			echo ${latestDates["$latest"]};;
+		*)
+			echo "Incorrect input."
+			return 1;;
+	esac
+}
